71c542b468ce71b0c683003da11e0e7db5717a28
xutopia <12456248836@qq.com>
2026-01-25 21:34:48 +0800

    initiate work

---
+++ src/renderer/src/components/AppEntry.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="app-entry">
+        <div class="home-entry">
+            <HomeNavigation />
+            <!-- <VideoPreview /> -->
+            <router-view class="page-view" />
+        </div>
+        <MessageToast />
+        <RecentMessagesPanel />
+    </div>
+</template>
+
+<script setup lang="ts">
+// import VideoPreview from './VideoPreview.vue'
+import HomeNavigation from './HomeNavigation.vue'
+import { useAppStore } from '../stores/AppStore'
+import RecentMessagesPanel from '@renderer/components/MessageNotify/RecentMessagesPanel.vue'
+import MessageToast from '@renderer/components/MessageNotify/MessageToast.vue'
+const appStore = useAppStore()
+import { onBeforeMount, onMounted, watch } from 'vue'
+import util from '../utils/util.js'
+import { IpcApi } from '../utils/ipcApi'
+import * as DataTypes from '../../../bridge/dataTypedef'
+import router from '../router/router'
+// 启动一个定时器，周期性trigger_event
+function startTimer(): void {
+    setInterval(() => {
+        const req: DataTypes.Req = {
+            cmd: 'heart_beat'
+        }
+        IpcApi.trigger_event<string, DataTypes.HeartBeat>(req)
+            .then((response: DataTypes.Resp<DataTypes.HeartBeat>) => {
+                util.process_heartbeat(response)
+            })
+            .catch((error: Error) => {
+                console.log('process_heartbeat failed', error)
+            })
+    }, 500)
+}
+
+watch(
+    () => appStore.documentTitle,
+    (docTitle: string | null) => {
+        if (docTitle === '' || docTitle === null) {
+            document.title = 'VideoPlayer'
+            return
+        }
+        document.title = 'VideoPlayer' + '  ' + docTitle
+        // // 获取当前时间毫秒
+        // const now = new Date()
+        // const nowStr = now.toLocaleString()
+        // // 获取当前时间的毫秒数
+        // const nowMs = now.getTime()
+        // console.log('document.title', nowStr, nowMs, document.title)
+    }
+)
+
+onBeforeMount(async () => {
+    util.setAppStore(appStore)
+    const resp = await util.start_app()
+    if (resp.code !== 0) {
+        return
+    }
+    startTimer()
+})
+
+onMounted(() => {
+    router.push('/')
+})
+</script>
+
+<style scoped>
+.app-entry {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+}
+
+.home-entry {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+}
+
+.page-view {
+    height: calc(100% - 30px);
+    width: 100%;
+    margin: 0;
+    padding: 0;
+}
+</style>
---
+++ src/renderer/src/components/CreatePrj.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="create-prj-comtainer">
+        <router-link to="/" class="no-underline-link">
+            <button class="xc-button">返回主页</button>
+        </router-link>
+        <br />
+        <div v-for="(repo, index) in dataRepo" :key="index" class="input-container">
+            <div>
+                <label>仓库路径：</label>
+                <input
+                    v-model="repo.path"
+                    class="xc-text-input"
+                    type="text"
+                    placeholder="请输入仓库路径"
+                    style="width: 80%"
+                />
+            </div>
+        </div>
+
+        <button class="xc-button" @click="btnclk_create_prj">创建项目</button>
+        <label>项目创建成功后，需要到 功能 界面中，项目同步</label>
+    </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+// import { useAppStore } from '../stores/AppStore'
+// const appStore = useAppStore()
+import '../assets/common.css'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+import * as DataTypes from '../../../bridge/dataTypedef'
+import util from '@renderer/utils/util'
+
+// 创建一个ref数组 3个元素
+const dataRepo = ref<DataTypes.DataRepo[]>([
+    {
+        name: 'test_data',
+        path: 'D:/02_workspace/05_timeCapsule/02_stream_manager/test_data',
+        thumbnailPath: ''
+    }
+])
+
+async function btnclk_create_prj(): Promise<void> {
+    let relRepo = dataRepo.value.filter((repo) => repo.name != null && repo.name !== '')
+
+    if (relRepo == null || relRepo.length === 0) {
+        return util.addToast(`请输入数据路径`, 'error')
+    }
+    const response = await util.create_prj(relRepo)
+    if (response.code === 0) {
+        if (response.bOver === false) {
+            util.addToast('创建项目中，请稍候...', 'warning')
+        } else {
+            util.addToast('创建项目成功', 'info')
+        }
+        router.push('/')
+    } else {
+        util.addToast(`创建项目失败: ${response.status}`, 'error')
+    }
+}
+</script>
+<style scoped>
+.create-prj-comtainer {
+    width: 100%;
+    height: calc(100% - var(--xc-home-nac-height));
+    margin: 0;
+    padding: 0;
+    background-color: var(--xc-background-color);
+    color: var(--xc-text-color);
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+.input-container {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: var(--xc-background-color);
+}
+</style>
