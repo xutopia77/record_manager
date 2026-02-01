a634ebf2065080cd4be74aeddf80d14903280897
xutopia <1256248836@qq.com>
2026-02-01 17:50:22 +0800

    dev test

--- src/renderer/src/components/VideoPreview.vue
+++ src/renderer/src/components/VideoPreview.vue
@@ -32,6 +32,7 @@ import PlayProgressBar from './video_view/PlayProgressBar.vue'
 import PlayCtrl from './video_view/PlayCtrl.vue'
 import { PlayReq } from '../utils/util'
 import { ref, onMounted, watch, onBeforeMount, computed, onUnmounted, onBeforeUnmount } from 'vue'
+// import MessageShow from '@renderer/components/util/MessageShow.vue'
 import util from '@renderer/utils/util'
 import { useAppStore } from '@renderer/stores/AppStore'
 const appStore = useAppStore()
